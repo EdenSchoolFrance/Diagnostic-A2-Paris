ec1a660b8fc118f6475776211b83c2ed581c6c8a
Najaxx123 <romain.brival@outlook.fr>
2025-09-14 11:43:46 +0200

    le commit pour le jeu

--- jeu.js
+++ jeu.js
@@ -1,3 +1,36 @@
+const $word = document.getElementById("mot")
+
+let randomName = prenoms[Math.floor(Math.random() *prenoms.length)]
+randomName = randomName
+        .normalize("NFD")
+        .toUpperCase()
+        .replace(/[\u0300-\u036f]/g, "");
+let playedLetters = []
+let wrongLetters = []
+
+console.log(randomName)
+console.log(randomName.length)
+
+
+$word.textContent = randomName
+    .split("")
+    .map(l => (playedLetters.includes(l) ? l : "_"))
+    .join(" ");
+
 function verifierLettre(lettre) {
+    
     console.log(lettre)
+    if(!playedLetters.includes(lettre)){
+        playedLetters.push(lettre)
+        if(randomName.includes(playedLetters)){
+            alert("bababoii")
+        } else {
+            wrongLetters.push(lettre)
+            console.log(wrongLetters)
+        }
+    } else {
+        alert("nuh uh")
+    }
 }
+
+console.log($word)
