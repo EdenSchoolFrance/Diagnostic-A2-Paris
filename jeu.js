ce70c0e8e092395e3c6120efbe6c9b0e05f6156b
watcher857 <ybendjebbour@edenschool.fr>
2025-09-14 23:20:26 +0200

    désolé j'ai de grosse d'ificulter en JS

--- jeu.js
+++ jeu.js
@@ -1,3 +1,18 @@
+const $img = document.querySelector("#img")
+const $mot = document.querySelector("#mot")
+const $vies = document.querySelector("#vies")
+const $lettresFausses = document.querySelector("#lettres-fausses")
+const $lettresJouees = document.querySelector("#lettres-jouees")
+const $restart = document.querySelector("#restart")
+
 function verifierLettre(lettre) {
     console.log(lettre)
+    $mot.textContent = lettre
+    $lettresFausses.textContent = lettre
+    $lettresJouees.textContent = lettre
+        $img.src = "imgs/img1.png"
 }
+
+$restart.addEventListener("click", (event) => {
+  location.reload()
+});
