26a825bda8cccd57e12fc71811e910fd2f422925
Nolann DOMAS <ndomas@edenschool.fr>
2025-09-15 09:35:29 +0200

    coucou

--- jeu.js
+++ jeu.js
@@ -1,27 +1,27 @@
 
-// Ici, je vais faire les configurations, comme les mots secrets etc etc
-let motSecret
-let affichageMot = []
-let vies
-let lettresJouees = []
-let lettresFausses = []
+// Here, I will make the configurations, such as secret words, etc.
+let secretWord
+let viewWord = []
+let lifes
+let playedLetter = []
+let falseLetter = []
 
 function init() {
 
-    // Ici je vais prendre la variable prenom qui viens de prenoms.js pour importer tout les prenoms
+    // Here I will take the variable firstname from firstnames.js to import all first names.
 
     const index = Math.floor(Math.random() * prenoms.length)
-    motSecret = prenoms[index].toUpperCase()
+    secretWord = prenoms[index].toUpperCase()
 
-    // Ici je fait un tableau vide qui prend mon mot secret et met des _ ( qui seront plus tard remplis par la lettre que j'ai choisis si elle est valide. )
-    affichageMot = []
-    for (let i = 0; i < motSecret.length; i++) {
-        affichageMot[i] = '_'
+    // Here, i do a blank table who contain the word ans ramplace "lettre" with _
+    viewWord = []
+    for (let i = 0; i < secretWord.length; i++) {
+        viewWord[i] = '_'
     }
 
-    lettresJouees = []
-    lettresFausses = []
-    vies = 11
+    playedLetter = []
+    falseLetter = []
+    lifes = 11
 
     const boutons = document.getElementsByClassName('lettre')
     for (let i = 0; i < boutons.length; i++) {
@@ -29,15 +29,15 @@ function init() {
     }
 }
 
-function verifierLettre(lettre) {
+function verifyLettres(lettre) {
     lettre = lettre.toUpperCase()
 
-    for (let i = 0; i < lettresJouees.length; i++) {
-        if (lettresJouees[i] === lettre) return
+    for (let i = 0; i < playedLetter.length; i++) {
+        if (playedLetter[i] === lettre) return
     }
 
-    lettresJouees.push(lettre)
-    document.getElementById('lettres-jouees').textContent = 'Lettres Jouees : ' + lettresJouees.join(' ')
+    playedLetter.push(lettre)
+    document.getElementById('lettres-jouees').textContent = 'Played Letters : ' + playedLetter.join(' ')
 
     const boutons = document.getElementsByClassName('lettre')
     for (let i = 0; i < boutons.length; i++) {
@@ -45,33 +45,33 @@ function verifierLettre(lettre) {
     }
 
     let trouvé = false
-    for (let i = 0; i < motSecret.length; i++) {
-        if (motSecret[i] === lettre) {
-            affichageMot[i] = lettre
+    for (let i = 0; i < secretWord.length; i++) {
+        if (secretWord[i] === lettre) {
+            viewWord[i] = lettre
             trouvé = true
         }
     }
 
-    document.getElementById('mot').textContent = affichageMot.join(' ')
+    document.getElementById('mot').textContent = viewWord.join(' ')
 
     if (!trouvé) {
-        lettresFausses.push(lettre)
-        document.getElementById('lettres-fausses').textContent = 'lettres fausses : ' + lettresFausses.join(' ')
+        falseLetter.push(lettre)
+        document.getElementById('lettres-fausses').textContent = 'False Letters : ' + falseLetter.join(' ')
 
-        // Je retire les vies quand c'est faux
-        vies--
-        document.getElementById('vies').textContent = 'vies restantes : ' + vies
-        document.getElementById('img').src = 'imgs/img' + vies + '.png'
+        // I delete a life when the choice is false
+        lifes--
+        document.getElementById('lifes').textContent = 'remaining lives : ' + lifes
+        document.getElementById('img').src = 'imgs/img' + lifes + '.png'
 
-        if (vies === 0) {
-            alert('perdu le mot etait : ' + motSecret)
-            document.getElementById('mot').textContent = motSecret
+        if (lifes === 0) {
+            alert('looser, the word is : ' + secretWord)
+            document.getElementById('mot').textContent = secretWord
             for (let i = 0; i < boutons.length; i++) boutons[i].disabled = true
         }
     } else {
         let gagné = true
-        for (let i = 0; i < affichageMot.length; i++) {
-            if (affichageMot[i] === '_') {
+        for (let i = 0; i < viewWord.length; i++) {
+            if (viewWord[i] === '_') {
                 gagné = false
                 break
             }
@@ -86,7 +86,7 @@ function verifierLettre(lettre) {
 
 
 
-// Je fait les fonction qui ont besoin de la fonction init ( comme pour restart la partie ou initialiser le jeu au lancement de la page)
+// here, i do the init request function
 
 function restart() {
     init()
